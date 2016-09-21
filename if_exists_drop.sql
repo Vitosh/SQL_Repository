fc9ae2d91530530c29c923eea4df9533e21d2a26
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-21 17:17:46 +0200

    Update if_exists_drop.sql

--- if_exists_drop.sql
+++ if_exists_drop.sql
@@ -1,2 +1,3 @@
 IF OBJECT_ID(N'TestTable', N'U') IS NOT NULL DROP TABLE TestTable;
 IF OBJECT_ID(N'TestSequence', N'SO') IS NOT NULL DROP SEQUENCE TestSequence;
+IF OBJECT_ID (N'TestView', N'V') IS NOT NULL DROP VIEW TestView;
