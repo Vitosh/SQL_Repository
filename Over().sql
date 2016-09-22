b6005df50ab76c141946c1278e9d55602b7096ff
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-22 17:10:32 +0200

    Update Over().sql

--- Over().sql
+++ Over().sql
@@ -5,6 +5,7 @@ SELECT
   P.productname, 
   P.unitprice, 
   AVG(p.unitprice) OVER(PARTITION BY s.companyname)[Supplier Average], 
-  AVG(p.unitprice) OVER(Partition by s.country)[Country Average]
+  AVG(p.unitprice) OVER(Partition by s.country)[Country Average],
+  AVG(p.unitprice) OVER() [AVG]
 FROM Production.Suppliers AS S
   LEFT OUTER JOIN Production.Products AS P ON S.supplierid = P.supplierid
