6f26744d9d8dc1d36fadb4bb18c0f4833ae4fd7c
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-22 17:26:12 +0200

    Update Over().sql

--- Over().sql
+++ Over().sql
@@ -9,3 +9,6 @@ SELECT
   AVG(p.unitprice) OVER() [AVG]
 FROM Production.Suppliers AS S
   LEFT OUTER JOIN Production.Products AS P ON S.supplierid = P.supplierid
+  --
+SELECT * , ROW_NUMBER() OVER(PARTITION BY companyname ORDER BY companyname) [JUST SO]
+FROM Production.Suppliers
