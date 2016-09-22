e4c58fa84d33409829c32f23d058a0558fae4ab4
Vitosh Academy <Vitosh@users.noreply.github.com>
2016-09-22 17:09:29 +0200

    Create OVER.sql

---
+++ OVER.sql
@@ -0,0 +1,10 @@
+SELECT
+  S.companyname AS supplier, 
+  S.country,  
+  P.productid, 
+  P.productname, 
+  P.unitprice, 
+  AVG(p.unitprice) OVER(PARTITION BY s.companyname)[Supplier Average], 
+  AVG(p.unitprice) OVER(Partition by s.country)[Country Average]
+FROM Production.Suppliers AS S
+  LEFT OUTER JOIN Production.Products AS P ON S.supplierid = P.supplierid
